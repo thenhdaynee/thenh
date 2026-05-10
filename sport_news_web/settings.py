f7f8692609eeaf26b0b0c67c36c388e61157bb18
Thenh Day Ne <thanhredmiturbo@gmail.com>
2026-05-11 00:37:13 +0700

    test cuoi

--- sport_news_web/settings.py
+++ sport_news_web/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jr#ruu_o)jexwy=tr-4*xddug)kxll(9#ksht$c+iqxud#d5)^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
