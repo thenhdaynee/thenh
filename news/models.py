cb24d46497065081d6ee67ffa816b302c9db8af6
Thenh Day Ne <thanhredmiturbo@gmail.com>
2026-05-16 02:01:26 +0700

    fixx

--- news/models.py
+++ news/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 from cloudinary.models import CloudinaryField
 
@@ -6,7 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name="Tiêu đề")
     content = models.TextField(verbose_name="Nội dung")
     image_url = models.URLField(blank=True, null=True, verbose_name="Hoặc dán link ảnh")
-    image_file = CloudinaryField('image', blank=True, null=True, verbose_name="Hoặc tải ảnh lên từ máy")
+    image_file = CloudinaryField('Hoặc tải ảnh lên từ máy', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
