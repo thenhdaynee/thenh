072a53aa5b170ba3dcbb7b32f3e965c321537279
Thenh Day Ne <thanhredmiturbo@gmail.com>
2026-06-09 01:36:47 +0700

    them database

--- sport_news_web/settings.py
+++ sport_news_web/settings.py
@@ -112,24 +112,19 @@ WSGI_APPLICATION = 'sport_news_web.wsgi.application'
 # =========================================================
 
 if os.environ.get('RENDER'):
-
     DATABASES = {
         'default': dj_database_url.config(
             default=os.environ.get('DATABASE_URL'),
             conn_max_age=600
         )
     }
-
 else:
-
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-
 # =========================================================
 # PASSWORD VALIDATORS
 # =========================================================
